e7ff248c79d717756b3339d6caeaf1ac5b49998c
clocktower39 <clocktower39@aol.com>
2021-06-03 14:51:50 -0700

    turn on tv and change to HDMI source

---
+++ tv-controller.js
@@ -0,0 +1,23 @@
+var CecController = require("cec-controller");
+var cecCtl = new CecController();
+
+cecCtl.on("ready", readyHandler);
+cecCtl.on("error", console.error);
+
+
+function readyHandler(controller)
+{
+	async function wakeUp()
+	{
+		await controller.dev0.turnOn();
+		console.log('Turned on TV');
+	
+		await controller.setActive();
+		console.log('Changed TV input source');
+	} 
+	
+	wakeUp();
+
+	// does not detect volume on old samsung and new phillips google tv
+	cecCtl.on('keypress', (keyName) => console.log(`User pressed: ${keyName}`));
+}
