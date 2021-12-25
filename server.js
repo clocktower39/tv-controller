3591057a8d4ea38863fdd1ed0ae9218359841e87
clocktower39 <clocktower39@aol.com>
2021-12-24 22:07:13 -0700

    single post route vs excessive get routes

--- server.js
+++ server.js
@@ -27,131 +27,15 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
 function readyHandler(controller) {
-  async function wakeUp() {
-    await controller.dev0.turnOn();
-    console.log("Turned on TV");
-
-  }
-
-  async function switchHDMI() {
-    await controller.setActive();
-    console.log("Changed TV input source");
-  }
-
-  async function sleep() {
-    await controller.dev0.turnOff();
-    console.log("Turned off TV");
-  }
-
-  async function moveDirection(direction) {
-    switch(direction) {
-      case 'up':
-      case 'down':
-      case 'left':
-      case 'right':
-        await controller.dev0.sendKey(direction);
-        console.log(`Arrow ${direction}`);
-        break;
-      default:
-        console.log('Incorrect direction submit, no action')
-        break;
-    }
-  }
+  app.post('/', async (req, res) => {
+    await controller.dev0.sendKey(req.body.key);
+    res.send(req.body.key)
+  })
 
   // uncomment below if not using react app
   // app.get("/", (req, res) => {
   //   res.sendFile(path.join(__dirname, "/index.html"));
   // });
-
-  app.get("/turn_on", (req, res) => {
-    wakeUp();
-    res.send("turned on tv");
-  });
-
-  app.get("/turn_off", (req, res) => {
-    sleep();
-    res.send("turned off tv");
-  });
-
-  app.get("/HDMIsource", (req, res) => {
-    switchHDMI();
-    res.send("turned on tv");
-  });
-
-  app.get("/up", (req, res) => {
-    moveDirection("up")
-    res.send("arrow up");
-  });
-
-  app.get("/down", (req, res) => {
-    moveDirection("down")
-    res.send("arrow down");
-  });
-
-  app.get("/left", (req, res) => {
-    moveDirection("left")
-    res.send("arrow left");
-  });
-
-  app.get("/right", (req, res) => {
-    moveDirection("right")
-    res.send("arrow right");
-  });
-
-  app.get("/vup", (req, res) => {
-    controller.volumeUp();
-    res.send("volume up");
-  });
-
-  app.get("/vdown", (req, res) => {
-    controller.volumeDown();
-    res.send("volume down");
-  });
-
-  app.get("/settings", async (req, res) => {
-    await controller.dev0.sendKey('setup-menu');
-    res.send("setup-menu");
-  });
-  app.get("/skip_previous", async (req, res) => {
-    await controller.dev0.sendKey('backward');
-    res.send("backward");
-  });
-  app.get("/rewind", async (req, res) => {
-    await controller.dev0.sendKey('rewind');
-    res.send("rewind");
-  });
-  app.get("/play", async (req, res) => {
-    await controller.dev0.sendKey('play');
-    res.send("play");
-  });
-  app.get("/fast_forward", async (req, res) => {
-    await controller.dev0.sendKey('fast-forward');
-    res.send('fast-forward');
-  });
-  app.get("/skip_next", async (req, res) => {
-    await controller.dev0.sendKey('forward');
-    res.send("forward");
-  });
-  app.get("/select", async (req, res) => {
-    await controller.dev0.sendKey('select');
-    res.send("select");
-  });
-  app.get("/mute", async (req, res) => {
-    await controller.dev0.sendKey('mute');
-    res.send("mute");
-  });
-  app.get("/back", async (req, res) => {
-    await controller.dev0.sendKey('exit');
-    res.send("exit");
-  });
-  app.get("/home", async (req, res) => {
-    await controller.dev0.sendKey('root-menu');
-    res.send("root-menu");
-  });
-  app.get("/menu", async (req, res) => {
-    await controller.dev0.sendKey('contents-menu');
-    res.send("contents-menu");
-  });
 
   io.on("connection", (socket) => {
     console.log("a user connected");
